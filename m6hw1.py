0ec275a2f859bcb3af22ebd9857c9c7822c61a67
koorlyyk <116065138+koorlyyk@users.noreply.github.com>
2023-02-07 06:48:25 +0300

    Update m6hw1.py
    update #2

--- m6hw1.py
+++ m6hw1.py
@@ -5,10 +5,11 @@ a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 for i in a:
     if i < 5:
         print(i)
+        
 #2
-my_dict = {'a': 500, 'b': 5874, 'c': 560,'d': 400, 'e': 5874, 'f': 20}
-my_dict = sorted(my_dict.values())
-print(my_dict[3:6])
+my_dict = {'a': 500, 'b': 5874, 'c': 560, 'd': 400, 'e': 5874, 'f': 20}
+print(sorted(my_dict, key=my_dict.get, reverse=True)[:3])
+
 
 #3
 rand = random.randint(1, 10000)
